47cbf326ad3f314dac3884e9a296c354c853f211
Jacobo Perez <jacoboviii@gmail.com>
2022-01-31 08:12:01 -0700

    Add required files for deployment

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
@@ -9,9 +11,11 @@ from resources.user import UserRegister
 from security import authenticate, identity
 
 app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://jacobo:Bernie!2021@192.168.0.5/FLASK_API"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "mysql+pymysql://jacobo:Bernie!2021@192.168.0.5/FLASK_API"
+)
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "ayla"
 api = Api(app)
